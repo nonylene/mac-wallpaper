b787a18bae8b40e39932525e17aecda4813e8277
nonylene <cookie.speaker@gmail.com>
2016-07-02 17:46:57 +0900

    use for each

--- wallpaper.js
+++ wallpaper.js
@@ -2,6 +2,9 @@ function run(argv) {
   if (argv.length !== 1) {
     throw Error("usage: osascript -l JavaScript wallpaper.js <filename>")
   }
-  var se = Application("System Events");
-  se.currentDesktop.picture.set(argv[0]);
+  var se = Application("System Events")
+  se.currentDesktop().picture.set(argv[0])
+  se.desktops().forEach(function(desktop) {
+    desktop.picture.set(argv[0]);
+  })
 }
