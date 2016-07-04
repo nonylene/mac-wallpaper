a949e802626598b83cf8e0335fcd7ab83ed69ffd
nonylene <cookie.speaker@gmail.com>
2016-07-05 01:23:50 +0900

    file path

--- everyday-changer.py
+++ everyday-changer.py
@@ -8,8 +8,10 @@ import config
 DATE_FILE = "last-updated.conf"
 DATE_FORMAT = "%Y/%m/%d %H:%M:%S"
 
+date_file_path = path.join(path.abspath(path.dirname(__file__)), DATE_FILE)
+
 try:
-    with open(DATE_FILE, "r") as f:
+    with open(date_file_path, "r") as f:
         last_date = datetime.strptime(f.readline().rstrip("\n"), DATE_FORMAT)
 except IOError:
     last_date = datetime.fromtimestamp(0)
@@ -18,5 +20,5 @@ current_date = datetime.now()
 if current_date.date() > last_date.date():
     result = subprocess.run(["./wallpaper.sh", config.URL], cwd = path.abspath(path.dirname(__file__)))
     if not result.returncode:
-        with open(DATE_FILE, "w") as f:
+        with open(date_file_path, "w") as f:
             f.write(current_date.strftime(DATE_FORMAT))
