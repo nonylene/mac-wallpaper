9b617be10fbc28593dea7db6a35971956d934f50
nonylene <cookie.speaker@gmail.com>
2016-07-11 21:56:38 +0900

    remove rebundant operation

--- wallpaper.js
+++ wallpaper.js
@@ -3,7 +3,6 @@ function run(argv) {
     throw Error("usage: osascript -l JavaScript wallpaper.js <filename>")
   }
   var se = Application("System Events")
-  se.currentDesktop().picture.set(argv[0])
   se.desktops().forEach(function(desktop) {
     desktop.picture.set(argv[0]);
   })
